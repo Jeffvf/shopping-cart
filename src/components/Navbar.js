fbf52c14399071377ba7c56f3824db574b34344d
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-11-20 17:48:46 -0300

    refactor: add shopping cart icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,17 @@
 import {Link} from "react-router-dom";
+import shoppingCart from "../assets/images/shopping-cart.png";
 
 function Navbar() {
   return(
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="shop">Shopping Page</Link>
+        <div className="nav-items-left">
+          <h3>Book store</h3>
+          <Link to="/">Home</Link>
+          <Link to="/shop">Shopping Page</Link>
+        </div>
+        <div className="nav-items-right">
+          <img src={shoppingCart} alt="shopping cart" width="36px" height="36px"></img>
+        </div>
     </nav>
   )
 }
