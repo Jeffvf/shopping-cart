c6a2de7219d2402194a50290f421d95603386f84
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-11-20 18:49:10 -0300

    feat: pass store items to child components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import ShopItem from "./pages/ShopItem";
+import bookData from "./utils/books.json";
+import uniqid from "uniqid";
+import {useEffect, useState} from "react";
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [displayStore, setDisplayStore] = useState('grid');
+
+  useEffect(() => {
+    setBooks(bookData.map((book) => {
+      return {...book, id: uniqid()}
+    }));
+  }, []);
+  
+  function uploadDisplayStore() {
+    setDisplayStore(displayStore === "grid" ? "none": "grid");
+  }
   return (
     <BrowserRouter>
       <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/shop" element={<Shop />}>
-            <Route path=":itemId" element={<ShopItem />}></Route>
+          <Route path="/shop" element={<Shop books={books} display={displayStore}/>}>
+            <Route path=":itemId" element={
+              <ShopItem books={books} displayStore={uploadDisplayStore}/>
+            }></Route>
           </Route>
       </Routes>
     </BrowserRouter>
