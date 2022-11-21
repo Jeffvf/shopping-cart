c23a353695799c6f7429cf95059b3b5478a17a32
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-11-21 00:11:59 -0300

    feat: implement shopping cart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import ShopItem from "./pages/ShopItem";
+import Cart from "./pages/Cart";
 import bookData from "./utils/books.json";
 import uniqid from "uniqid";
 import {useEffect, useState} from "react";
@@ -9,6 +10,7 @@ import {useEffect, useState} from "react";
 function App() {
   const [books, setBooks] = useState([]);
   const [displayStore, setDisplayStore] = useState('grid');
+  const [cartItems, setCartItems] = useState([]);
 
   useEffect(() => {
     setBooks(bookData.map((book) => {
@@ -19,15 +21,21 @@ function App() {
   function uploadDisplayStore() {
     setDisplayStore(displayStore === "grid" ? "none": "grid");
   }
+
+  function addItemToCart(item) {
+    setCartItems([...cartItems, item]);
+  }
+
   return (
     <BrowserRouter>
       <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/shop" element={<Shop books={books} display={displayStore}/>}>
+          <Route path="/shop" element={<Shop books={books} display={displayStore} fn={addItemToCart}/>}>
             <Route path=":itemId" element={
               <ShopItem books={books} displayStore={uploadDisplayStore}/>
             }></Route>
           </Route>
+          <Route path="/cart" element={<Cart items={cartItems}/>}></Route>
       </Routes>
     </BrowserRouter>
   );
