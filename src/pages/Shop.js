9c569415ac38385e33065be4ccfa3f01c238186a
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-11-21 00:10:51 -0300

    feat: button that inserts item to cart

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,17 +1,21 @@
 import { Outlet, Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Card from "../components/Card";
+import Button from "../components/Button";
 
-function Shop({books, display}) {
+function Shop({books, display, fn}) {
   return(
     <div>
       <Navbar />
       <div className="store" style={display= {display}}>
         {books.map((book) => {
           return (
-            <Link key={book.id} to={book.id.toString()}>
-              <Card book={book} />
-            </Link>
+            <div key={book.id} className="store-item">
+              <Link to={book.id.toString()}>
+                <Card book={book} />
+              </Link>
+              <Button text="Add to cart" action={() => fn(book)}/>
+            </div>
           )
         })}
       </div>
